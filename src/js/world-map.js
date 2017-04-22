e09e777895b4c6df534599968687b80314b95637
Tyler Parker <btylerparker@gmail.com>
2017-04-22 14:37:42 -0400

    Remove console log

--- src/js/world-map.js
+++ src/js/world-map.js
@@ -1,8 +1,6 @@
 // map.js
 const WorldMap = function(game, tileset, tiles, tilemap) {
   this.map = game.add.tilemap(tilemap);
-  console.log(tileset);
-  console.log(tiles);
   this.map.addTilesetImage(tileset, tiles);
   this.backgroundlayer = this.map.createLayer('BackgroundLayer');
   this.collisionLayer = this.map.createLayer('CollisionLayer');
