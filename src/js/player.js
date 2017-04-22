1e38ec3c3b7d398b8e0919550afca11a7e280cec
Matt Dannenberg <matt.dannenberg@teacherspayteachers.com>
2017-04-22 14:18:46 -0400

    so... this is hunger?

--- src/js/player.js
+++ src/js/player.js
@@ -1,9 +1,27 @@
 const Actor = require('./actor');
 
 const MOVE_SPEED = 150;
+
+const HUNGER_INCREASE_RATE = 200; // millis
+// How many hunger before death.
+const STARVING_TO_DEATH = 8;
+
 const Player = function(game) {
   const x = 100;
   const y = 100;
+
+  // Used to track hunger. Half sandwhich to represent one level.
+  this.hungerLevel = 0;
+
+  this.hungerTimer = game.time.create(false);
+  this.hungerTimer.add(
+    HUNGER_INCREASE_RATE, // delay
+    this.hungerCallback, // callback
+    this, // callbackContext
+    null // arguments
+  );
+  this.hungerTimer.start();
+  console.log(this.hungerTimer);
 
   Actor.call(this, game, x, y, 'player');
   game.physics.arcade.enable(this);
@@ -31,4 +49,44 @@ Player.prototype.updateControls = function (cursors) {
     this.body.velocity.y = 0;
   }
 };
+
+Player.prototype.eatAFood = function (a_food) {
+  this.hungerTimer.clearPendingEvents();
+  this.hungerLevel -= a_food.calories;
+  if (this.hungerLevel < 0) {
+    this.hungerLevel = 0;
+  }
+  this.hungerTimer.add(
+    HUNGER_INCREASE_RATE, // delay
+    this.hungerCallback, // callback
+    this, // callbackContext
+    null // arguments
+  );
+}
+
+Player.prototype.getCurrentHungerLevel = function () {
+  return this.hungerLevel;
+}
+
+Player.prototype.getMaxHungerLevel = function () {
+  return STARVING_TO_DEATH;
+}
+
+Player.prototype.hungerCallback = function () {
+  console.log("I LOGGED IT CALLED>>> BACK");
+  this.hungerTimer.clearPendingEvents();
+  this.hungerLevel++;
+  if (this.hungerLevel >= STARVING_TO_DEATH) {
+    console.log("OH NOOOOO YOU STARVED TO DEATH :(((");
+  } else {
+    console.log("ooooo so hungry...");
+    this.hungerTimer.add(
+      HUNGER_INCREASE_RATE, // delay
+      this.hungerCallback, // callback
+      this, // callbackContext
+      null // arguments
+    );
+  }
+}
+
 module.exports = Player
