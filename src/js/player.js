cd7dd49136f5dbaa968f921d510f9df97a3df9fd
Brian Shaginaw <brian.shaginaw@warbyparker.com>
2017-04-22 13:14:41 -0400

    Can move player around a lil.

--- src/js/player.js
+++ src/js/player.js
@@ -1,12 +1,32 @@
 // player.js
+const MOVE_SPEED = 150;
 const Player = function(game) {
   const x = 100;
   const y = 100;
   Phaser.Sprite.call(this, game, x, y, 'player');
+  game.physics.arcade.enable(this);
 }
 Player.prototype = Object.create(Phaser.Sprite.prototype);
 Player.prototype.constructor = Player;
 
 Player.prototype.updateControls = function (cursors) {
+  if (cursors.left.isDown) {
+    this.body.velocity.x = -MOVE_SPEED;
+  }
+  else if (cursors.right.isDown) {
+    this.body.velocity.x = MOVE_SPEED;
+  }
+  else {
+    this.body.velocity.x = 0;
+  }
+  if (cursors.up.isDown) {
+    this.body.velocity.y = -MOVE_SPEED;
+  }
+  else if (cursors.down.isDown) {
+    this.body.velocity.y = MOVE_SPEED;
+  }
+  else {
+    this.body.velocity.y = 0;
+  }
 };
 module.exports = Player
