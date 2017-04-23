e07dec51d7461fadbd3ed4513c9885e55a643e3e
Tyler Parker <btylerparker@gmail.com>
2017-04-23 15:55:31 -0400

    New ants loading

--- src/js/ants/yellow-ant.js
+++ src/js/ants/yellow-ant.js
@@ -6,7 +6,7 @@ const YellowAnt = function(game, x, y) {
   Ant.call(this, game, x, y, 'ant_yellow_flying');
 };
 
-YellowAnt.prototype = Object.create(YellowAnt.prototype);
+YellowAnt.prototype = Object.create(Ant.prototype);
 YellowAnt.prototype.constructor = YellowAnt;
 YellowAnt.OBJECT_LAYER_NAME = OBJECT_LAYER_NAME;
 
