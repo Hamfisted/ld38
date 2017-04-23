4a29761a1254c0c8694dc39015a806dffd0316d7
Greg <gregt87@gmail.com>
2017-04-23 18:13:28 -0400

    swapped image and number

--- src/js/lib/insectPartCounter.js
+++ src/js/lib/insectPartCounter.js
@@ -13,19 +13,19 @@ function insectPartCounter(game, dimensions, insectPartColor) {
     wordWrap: true
   };
 
-  const textObj = game.add.text(0, 0, '0 x', style);
+  const textObj = game.add.text(18, 0, 'x 0', style);
 
-  const sprite = game.make.sprite(20, 0, 'ant_' + insectPartColor + '_part');
+  const sprite = game.make.sprite(0, 0, 'ant_' + insectPartColor + '_part');
   const scalingParameter = height / sprite.height;
   sprite.scale.setTo(scalingParameter , scalingParameter);
 
-  itemGroup.add(textObj)
   itemGroup.add(sprite)
+  itemGroup.add(textObj)
 
   return {
     update: function(player) {
       const number = player.insectParts[insectPartColor];
-      textObj.text = number + " x";
+      textObj.text = "x " + number;
     },
     group: itemGroup
   }
