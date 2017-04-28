342af218f826cb4c1ca356cbecb597f81269c35a
Greg <gregt87@gmail.com>
2017-04-28 00:25:31 -0400

    items hover

--- src/js/pickup.js
+++ src/js/pickup.js
@@ -3,8 +3,23 @@ const Pickup = function(game, x, y, imageName) {
   this.is_picked_up = false
   Phaser.Sprite.call(this, game, x, y, imageName);
   game.physics.arcade.enable(this);
+
+  this.originalX = x;
+  this.originalY = y;
 }
+
+
 Pickup.prototype = Object.create(Phaser.Sprite.prototype);
 Pickup.prototype.constructor = Pickup;
+
+Pickup.prototype.update = function () {
+  Phaser.Sprite.prototype.update.call(this);
+
+  const seconds = new Date().getTime() / 1000;
+  const movementsPerSecond = 10;
+  const height = 4;
+
+  this.body.y = this.originalY + Math.sin(seconds * movementsPerSecond) * (height / (2 * Math.PI));
+};
 
 module.exports = Pickup
